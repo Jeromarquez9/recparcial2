c38373613bfc6de34ccb1db11624e1cbd02e274a
Jeromarquez9 <jeromarquez9m@gmail.com>
2025-12-03 19:54:00 -0300

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,1 +1,29 @@
+public class Stack {
 
+    private LinkedList list = new LinkedList();
+
+    public void push(Object data) {
+        list.add(data);
+    }
+
+    public Object pop() {
+        if (list.isEmpty()) return null;
+
+        Object last = list.get(list.size() - 1);
+        list.remove(last);
+        return last;
+    }
+
+    public Object peek() {
+        if (list.isEmpty()) return null;
+        return list.get(list.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int size() {
+        return list.size();
+    }
+}
